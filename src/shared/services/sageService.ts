237041bd54b6f2d84561f2ab6387a7a70e5bcb86
John Reilly <johnny_reilly@hotmail.com>
2017-04-22 22:33:47 +0100

    sageService ready

---
+++ src/shared/services/sageService.ts
@@ -0,0 +1,33 @@
+import { getConnectionUrl, status, json, makeFormData } from "./connection";
+import { Sage } from "../domain/dtos/sage";
+import { loadedSage, loadedSages, removedSage, savedSage, saveFailed } from "../actions/sageActions";
+
+const rootUrl = getConnectionUrl + "sage";
+
+export function getAll() {
+    return fetch(this.rootUrl)
+        .then(status)
+        .then(response => json<Sage[]>(response))
+        .then(loadedSages);
+}
+
+export function getById(id: number) {
+    return fetch(`${rootUrl}/id`)
+        .then(status)
+        .then(response => json<Sage>(response))
+        .then(loadedSage);
+}
+
+export function remove(id: number) {
+    return fetch(`${rootUrl}/id`, { method: "DELETE" })
+        .then(status)
+        .then(_ => removedSage(id));
+}
+
+export function save(sage: Sage) {
+    return fetch(rootUrl, { method: "POST", body: makeFormData(sage) })
+        .then(status)
+        .then(response => json<number>(response))
+        .then(savedSage)
+        .catch(saveFailed);
+}
